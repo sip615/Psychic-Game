4647a8d7dc943a09bac14be2eef4e92a5b52abd4
sip615 <sunilpatel.328@gmail.com>
2019-03-31 20:58:31 -0400

    update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,10 @@
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-  , randomLetter = ""
-  , winCount = 0
-  , lossCount = 0
-  , guessesLeft = 0
-  , guessedLetters = []
-  , defaultGuessesLeft = 9;
+   randomLetter = "";
+   winCount = 0;
+   lossCount = 0;
+   guessesLeft = 0;
+   guessedLetters = [];
+   defaultGuessesLeft = 9;
 
 window.addEventListener("keypress", onKeyPress, false);
 newGame();
